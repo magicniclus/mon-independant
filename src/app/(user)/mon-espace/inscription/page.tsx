d714ef282117c4eea9b26848d57a8757a7dbab20
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-14 15:32:55 +0100

    Create page.tsx

---
+++ src/app/(user)/mon-espace/inscription/page.tsx
@@ -0,0 +1,14 @@
+const page = () => {
+  return (
+    <>
+      <header className="w-full bg-slate-200">
+        <div className="max-w-7xl mx-auto">
+          <h1 className="text-blue-600">Inscription</h1>
+        </div>
+      </header>
+      <main></main>
+    </>
+  );
+};
+
+export default page;

9f89adbb243d3fe5871025544e76383a5abadcfc
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-14 16:50:29 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import NavBar from "../components/NavBar";
+
 export default function Home() {
   return (
     <>
-      <header className="w-full bg-slate-200">
-        <div className="max-w-7xl mx-auto">
-          <h1 className="text-blue-600">Accueil</h1>
-        </div>
+      <header className="w-full bg-slate-100">
+        <NavBar />
       </header>
       <main></main>
     </>

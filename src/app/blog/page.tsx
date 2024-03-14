2411e12bf4e7ef02a75a3047848a59b02d9194b2
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-14 16:50:26 +0100

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,7 @@
 const page = () => {
   return (
     <>
-      <header className="w-full bg-slate-200">
-        <div className="max-w-7xl mx-auto">
-          <h1 className="text-blue-600">Blog</h1>
-        </div>
-      </header>
+      <header className="w-full bg-slate-200"></header>
       <main></main>
     </>
   );

818d165e1ac89f5ed1f62235f7b05d23ec2f6211
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-14 15:33:01 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <header className="w-full bg-slate-200">
+        <div className="max-w-7xl mx-auto">
+          <h1 className="text-blue-600">Accueil</h1>
+        </div>
+      </header>
+      <main></main>
+    </>
+  );
 }

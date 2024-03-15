74f03bcf953f184bf1ad2577681ea49bac81e253
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-15 19:41:02 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,35 +22,35 @@ const Hero = () => {
           z-index: 1;
         }
       `}</style>
-      <section className="w-full bg-[url('/background/woman.jpg')] md:py-24 bg-no-repeat bg-cover bg-right relative background text-slate-700">
+      <section className="w-full bg-[url('/background/woman.jpg')] py-10 md:py-24 bg-no-repeat bg-cover bg-center relative background text-slate-700">
         <div className="px-6 lg:px-8 max-w-6xl mx-auto flex md:flex-row flex-col z-10 content">
           <div className="md:w-1/2 w-full">
-            <h1 className=" text-white font-bold text-6xl">
+            <h1 className=" text-white font-bold sm:text-6xl text-4xl">
               Créez votre <br /> auto-entreprise <br /> en quelques cliques
             </h1>
             <ul className=" text-white mt-8">
-              <li className="mt-3 flex items-center">
-                <CheckBadgeIcon className="h-7 w-auto text-green-700 mr-3" />{" "}
+              <li className="mt-3 flex items-center  md:text-normal text-xs">
+                <CheckBadgeIcon className="min-w-[12px] h-4 md:h-7 w-auto text-green-700 mr-3" />
                 Déclaration simplifiée au Statut Auto Entrepreneur 2024
               </li>
-              <li className="mt-3 flex items-center">
-                <CheckBadgeIcon className="h-7 w-auto text-green-700 mr-3" />
+              <li className="mt-3 flex items-center md:text-normal text-xs">
+                <CheckBadgeIcon className="min-w-[12px] h-4 md:h-7 w-auto text-green-700 mr-3" />
                 Déclaration simple et rapide
               </li>
-              <li className="mt-3 flex items-center">
+              <li className="mt-3 flex items-center md:text-normal text-xs">
                 {" "}
-                <CheckBadgeIcon className="h-7 w-auto text-green-700 mr-3" />
+                <CheckBadgeIcon className="min-w-[12px] h-4 md:h-7 w-auto text-green-700 mr-3" />
                 Dossier traité sous 24h
               </li>
             </ul>
           </div>
           <div className="md:w-1/2 w-full flex justify-center">
-            <form className="w-[80%] py-8 bg-white/85 rounded-md px-8 flex-col items-center">
+            <form className="md:w-[80%] w-full py-8 bg-white md:bg-white/85 rounded-md px-5 md:px-8 flex-col items-center md:mt-0 mt-7">
               <h2 className="text-center font-semibold w-9/12 mx-auto">
                 Formulaire de démarche en ligne simplifié
               </h2>
               <div className=" flex justify-between mt-5">
-                <div className="w-[45%]">
+                <div className="w-full md:w-[45%]">
                   <label htmlFor="nom" className="text-slate-700">
                     Nom
                   </label>
@@ -60,7 +60,7 @@ const Hero = () => {
                     className="w-full border px-2 py-1 rounded-md border-slate-400 mt-2"
                   />
                 </div>
-                <div className="w-[45%]">
+                <div className="w-[45%] md:block hidden">
                   <label htmlFor="prenom" className="text-slate-700">
                     Prénom
                   </label>
@@ -70,6 +70,16 @@ const Hero = () => {
                     className="w-full border px-2 py-1 rounded-md border-slate-400 mt-2"
                   />
                 </div>
+              </div>
+              <div className="w-full md:hidden block mt-3">
+                <label htmlFor="prenom" className="text-slate-700">
+                  Prénom
+                </label>
+                <input
+                  type="text"
+                  placeholder="Votre prénom"
+                  className="w-full border px-2 py-1 rounded-md border-slate-400 mt-2"
+                />
               </div>
               <div className="w-full mt-3">
                 <label htmlFor="email" className="text-slate-700">
@@ -88,7 +98,7 @@ const Hero = () => {
                   name="cgu"
                   className="mr-2 cursor-pointer"
                 />
-                <label htmlFor="cgu" className="text-slate-700 ml-3 text-xs">
+                <label htmlFor="cgu" className="text-slate-700 ml-2 text-xs">
                   J&apos;accepte les conditions générales de vente ainsi que la
                   politique de confidentialité
                 </label>

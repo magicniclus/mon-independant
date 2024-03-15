eb27ba5f4c5235b2779767757a5d712111600dda
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-15 14:40:45 +0100

    Update page.tsx

--- src/app/(user)/mon-espace/page.tsx
+++ src/app/(user)/mon-espace/page.tsx
@@ -1,11 +1,9 @@
+import NavBar from "@/components/TailwindUi/NavBar";
+
 const page = () => {
   return (
     <>
-      <header className="w-full bg-slate-200">
-        <div className="max-w-7xl mx-auto">
-          <h1 className="text-blue-600">Mon Espace</h1>
-        </div>
-      </header>
+      <NavBar />
       <main></main>
     </>
   );

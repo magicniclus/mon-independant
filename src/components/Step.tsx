086235ee7a9d41282a4c63cce559cb347ab6e46b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-20 21:57:26 +0100

    Create Step.tsx

---
+++ src/components/Step.tsx
@@ -0,0 +1,14 @@
+const Step = () => {
+  return (
+    <section className="md:w-4/12 w-full bg-slate-100 min-h-[500px] rounded-md py-10 text-slate-700">
+      <div className="px-6 lg:px-8">
+        <h2 className="font-semibold text-md text-green-700">
+          Numero de dossier
+        </h2>
+        <h3 className="text-md">32045393</h3>
+      </div>
+    </section>
+  );
+};
+
+export default Step;

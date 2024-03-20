81a119c27a2eff0bd42da5dae59c15273bab0b9c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-20 11:29:21 +0100

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,8 @@
-import { combineReducers, createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./createUserSlice";
-// Importez d'autres réducteurs si nécessaire
 
-// Combine tous vos réducteurs
-const rootReducer = combineReducers({
-  createUser: userReducer,
+export const store = configureStore({
+  reducer: {
+    createUser: userReducer,
+  },
 });
-
-// Créez et exportez le type RootState basé sur le rootReducer
-export type RootState = ReturnType<typeof rootReducer>;
-
-// Créez et exportez votre store
-export const store = createStore(rootReducer);
-
-// Vous pouvez également avoir besoin d'exporter le rootReducer selon votre configuration
-export default rootReducer;

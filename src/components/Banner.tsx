38d87aab663760aaba957b9e1c69d292283cea53
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-20 15:49:51 +0100

    Create Banner.tsx

---
+++ src/components/Banner.tsx
@@ -0,0 +1,13 @@
+const Banner = () => {
+  return (
+    <section className="w-full bg-green-700 text-white">
+      <div className="mx-auto w-full text-center flex max-w-6xl items-center p-6 lg:px-8 flex justify-center">
+        <h2 className="text-lg">
+          Formulaire de déclation d&apos;activité auto-entrepreneur
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;

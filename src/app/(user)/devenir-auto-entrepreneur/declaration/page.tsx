8185c6f097e44b9b3682fd31e572fa880e0a5432
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-20 21:57:18 +0100

    Update page.tsx

--- src/app/(user)/devenir-auto-entrepreneur/declaration/page.tsx
+++ src/app/(user)/devenir-auto-entrepreneur/declaration/page.tsx
@@ -1,12 +1,19 @@
 import Banner from "@/components/Banner";
 import NavBarLanding from "@/components/NavBarLanding";
+import Step from "@/components/Step";
 
 const page = () => {
   return (
     <>
       <NavBarLanding />
-      <Banner />
-      <main></main>
+      <main>
+        <Banner />
+        <div className="w-screen">
+          <div className="max-w-6xl mx-auto mt-10">
+            <Step />
+          </div>
+        </div>
+      </main>
     </>
   );
 };

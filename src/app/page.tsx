e2d9106cd5aaae868fd2c09cf836e603dd9a1a5e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-20 11:01:23 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import Hero from "@/components/Hero";
 import NavBar from "@/components/TailwindUi/NavBar";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <main>
-        <Hero />
-      </main>
+      <main>{/* <Hero /> */}</main>
     </>
   );
 }

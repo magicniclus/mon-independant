73c52d94e50f7d563a52c9ba749904b2cd05c9c3
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-23 15:18:04 +0100

    Update page.tsx

--- src/app/(user)/devenir-auto-entrepreneur/declaration/page.tsx
+++ src/app/(user)/devenir-auto-entrepreneur/declaration/page.tsx
@@ -1,6 +1,7 @@
 import Banner from "@/components/Banner";
 import NavBarLanding from "@/components/NavBarLanding";
 import Step from "@/components/Step";
+import StepManager from "@/components/StepManager";
 
 const page = () => {
   return (
@@ -9,8 +10,9 @@ const page = () => {
       <main>
         <Banner />
         <div className="w-screen">
-          <div className="max-w-6xl mx-auto mt-10">
+          <div className="max-w-6xl mx-auto mt-10 flex justify-between px-6 lg:px-8 mb-20">
             <Step />
+            <StepManager />
           </div>
         </div>
       </main>

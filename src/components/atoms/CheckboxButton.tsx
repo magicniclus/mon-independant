ff8838a78e52b9cc6bf7391d300197e835a792fd
Nicolas <72094122+magicniclus@users.noreply.github.com>
2024-03-27 15:48:04 +0100

    Update CheckboxButton.tsx

--- src/components/atoms/CheckboxButton.tsx
+++ src/components/atoms/CheckboxButton.tsx
@@ -1,5 +1,21 @@
+"use client";
+
+import Script from "next/script";
+import Stripe from "stripe";
+
 const CheckboxButton = () => {
-  return <></>;
+  const handleCheckout = async () => {
+    const res = await fetch("/api/checkout", { method: "POST" });
+    const data = await res.json();
+    Stripe.redirectToCheckout({ sessionId: data.sessionId });
+  };
+
+  return (
+    <>
+      <Script src="https://js.stripe.com/v3/"></Script>
+      <button onClick={handleCheckout}>Acheter maintenant</button>
+    </>
+  );
 };
 
 export default CheckboxButton;
